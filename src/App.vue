3c1be7f3609e9178f725caf0ab605933db91c512
Danielhenrique123 <danielhenrique090109@gmail.com>
2025-05-30 15:09:55 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+<header>
+
+<nav>
+
+<h1>
+  <a href="#">
+    IFbooks
+
+    <span class="logo-title">Apreço a livros</span>
+
+    <div class="search-wrapper">
+
+      <input type="text" class="search" placeholder="Buscar...">
+
+
+    </div>
+
+    <ul>
+
+
+      <li>Termos</li>
+      <li>Equipe</li>
+      <li>Envio</li>
+      <li>Devoluções</li>
+
+      
+    </ul>
+
+
+  </a>
+</h1>
+
+</nav>
+
+
+</header>
+</template>
